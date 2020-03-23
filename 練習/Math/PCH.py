7cce408ad0bc6ca42cfe01310b630b0351101b40
r91090701 <r91090701@gmail.com>
2020-03-23 23:03:25 +0800

    新增PCH的包裹

---
+++ 練習/Math/PCH.py
@@ -0,0 +1,7 @@
+
+def P(a,b):
+    print("t")
+def C(a,b):
+    print("t")
+def H(a,b):
+    print("t")
--- 練習/練習.py
+++ 練習/練習.py
@@ -1,6 +1,7 @@
 import sys
-import Math.distance as M
+import Math.distance as D
+import Math.PCH as PCH
 print("點(d,e)  L:ax+by+c=0,輸入a,b,c,d,e來計算點到直線的距離")
 for n in sys.stdin:
     a,b,c,d,e=n.split()
-    print(M.Dis(a,b,c,d,e))
+    print(D.Dis(a,b,c,d,e))
